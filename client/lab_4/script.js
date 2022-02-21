7ca1dc52ee1e04b822272b80512b1417a6214941
Cameron Martin <camumd15@gmail.com>
2022-02-20 20:54:42 -0500

    lab 4 update

---
+++ client/lab_4/script.js
@@ -0,0 +1,9 @@
+const slidePosition = 0;
+const slides = document.querySelectorAll('carousel_item');
+const totalSlides = slides.length;
+
+document
+  .document.querySelector('carousel_button--next')
+  .addEventListener('click', () => {
+    console.log('helllllo');
+  });

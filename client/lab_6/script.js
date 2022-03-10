db4ad7d204cb03f385c39bc1e7f12d587c66f507
Cameron Martin <camumd15@gmail.com>
2022-03-09 19:34:43 -0500

    lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,14 +1,34 @@
+// As the last step of your lab, hook this up to index.html
+function generateRandomInt(min,max){
+  return Math.floor((Math.random() * (max+1 -min)) +min);
+}
+function datahandler(array) {
+  const target = document.querySelector('.restaurants');
+  target.innerHTML = '';
+  const range = [...Array(15).keys()];
+  const rest = range.map(elm=>{
+    const index = generateRandomInt(0,array.length);
+    return array[index];
+  })
+  rest.forEach(elm=>{
+    const str = `<li>${elm.name}</li>`;
+    target.innerHTML+= str;
+  })
+}
+
+
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
+    const submit = document.querySelector('button');
     const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
     const arrayFromJson = await results.json(); // This changes it into data we can use - an object
     console.table(arrayFromJson.data); // this is called "dot notation"
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
-  });
+    // it contains all 1,000 records we need 
+    submit.style.display = 'block';
+    if(arrayFromJson.data.length > 0){
+      datahandler(arrayFromJson.data)
+    }
 }
 
 // this actually runs first! It's calling the function above
